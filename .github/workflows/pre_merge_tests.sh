297875f5b1c015f4d79de54fa64c29c67c15b623
Julio Gajardo Peredo <oilujulio@gmail.com>
2025-01-28 20:24:54 -0300

    FIX: fixed exit status code for tests

--- .github/workflows/pre_merge_tests.sh
+++ .github/workflows/pre_merge_tests.sh
@@ -3,15 +3,15 @@
 echo "Running pre-merge script..."
 
 echo "***** RUNNING UNIT TESTS *****"
-npm run test:unit -- run || return 1
+npm run test:unit -- run || exit 1
 
 echo "***** RUNNING LINTER *****"
-npm run lint || return 1
+npm run lint || exit 1
 
 echo "***** RUNNING CHECK *****"
-npm run check || return 1
+npm run check || exit 1
 
-npm run build || return 1
+npm run build || exit 1
 
 echo "*** Finished tests for $dir ***"
 
