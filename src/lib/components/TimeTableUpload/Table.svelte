a508ef6d4ebeaf795b1cefe78d2483e52d6d1f8f
Julio Gajardo Peredo <oilujulio@gmail.com>
2025-02-03 19:19:04 -0300

    REFACTOR: reduced component complexity

--- src/lib/components/TimeTableUpload/Table.svelte
+++ src/lib/components/TimeTableUpload/Table.svelte
@@ -1,21 +1,97 @@
 <script lang="ts">
+  import type { WeeklyLoad } from '$lib/state/Timetable.svelte';
+
   const {
-    grades,
-    teachers,
-    subjects,
-    tableByTeacher,
-    tableByGrade,
-    tableBySubject,
+    weeklyLoad,
     by
   }: {
-    grades: Set<string>;
-    teachers: Set<string>;
-    subjects: Set<string>;
-    tableByTeacher: Record<string, Record<string, number>>;
-    tableByGrade: Record<string, number>;
-    tableBySubject: Record<string, Record<string, number>>;
+    weeklyLoad: WeeklyLoad[];
     by: 'subject' | 'teacher';
   } = $props();
+
+  const getTableByTeacher = (
+    load: WeeklyLoad[]
+  ): Record<string, Record<string, number>> | never => {
+    // assert each pair of teacher and grade is unique
+    const teacherSubjectPairs = new Set<string>();
+    const table: Record<string, Record<string, number>> = {};
+
+    load.forEach((lesson) => {
+      const { teacherName, gradeName, subjectName, weeklyLoad } = lesson;
+      const teacherSubject = `${teacherName} ${gradeName} ${subjectName}`;
+      if (teacherSubjectPairs.has(teacherSubject)) {
+        throw new Error(`Duplicado de par de profesor y materia: ${teacherSubject}`);
+      }
+      teacherSubjectPairs.add(teacherSubject);
+
+      if (!table[teacherName]) {
+        table[teacherName] = {};
+      }
+
+      if (!table[teacherName][gradeName]) {
+        table[teacherName][gradeName] = 0;
+      }
+
+      table[teacherName][gradeName] += weeklyLoad;
+    });
+
+    return table;
+  };
+
+  const getTableByGrade = (load: WeeklyLoad[]): Record<string, number> | never => {
+    const table: Record<string, number> = {};
+
+    load.forEach((lesson) => {
+      const { gradeName, weeklyLoad } = lesson;
+      if (!table[gradeName]) {
+        table[gradeName] = 0;
+      }
+
+      table[gradeName] += weeklyLoad;
+    });
+
+    return table;
+  };
+
+  const getTableBySubject = (
+    load: WeeklyLoad[]
+  ): Record<string, Record<string, number>> | never => {
+    const table: Record<string, Record<string, number>> = {};
+
+    load.forEach((lesson) => {
+      const { gradeName, subjectName, weeklyLoad } = lesson;
+      if (!table[subjectName]) {
+        table[subjectName] = {};
+      }
+
+      if (!table[subjectName][gradeName]) {
+        table[subjectName][gradeName] = 0;
+      }
+
+      table[subjectName][gradeName] += weeklyLoad;
+    });
+
+    return table;
+  };
+
+  const getTeachers = (load: WeeklyLoad[]): Set<string> => {
+    return new Set(load.map((lesson) => lesson.teacherName));
+  };
+
+  const getGrades = (load: WeeklyLoad[]): Set<string> => {
+    return new Set(load.map((lesson) => lesson.gradeName));
+  };
+
+  const getSubjects = (load: WeeklyLoad[]): Set<string> => {
+    return new Set(load.map((lesson) => lesson.subjectName));
+  };
+
+  const tableByTeacher = getTableByTeacher(weeklyLoad);
+  const tableBySubject = getTableBySubject(weeklyLoad);
+  const tableByGrade = getTableByGrade(weeklyLoad);
+  const teachers = getTeachers(weeklyLoad);
+  const grades = getGrades(weeklyLoad);
+  const subjects = getSubjects(weeklyLoad);
 </script>
 
 <table class="table-auto border-collapse border text-center">
--- src/lib/components/TimeTableUpload/TimeTableUpload.svelte
+++ src/lib/components/TimeTableUpload/TimeTableUpload.svelte
@@ -9,14 +9,7 @@
   let timetableName = $state<string>('');
   let timetablePeriods = $state<Period>(7);
   let timetableFile = $state<File | null>(null);
-
   let weeklyLoad = $state<WeeklyLoad[] | null>(null);
-  let tableByTeacher = $state<Record<string, Record<string, number>>>({});
-  let tableBySubject = $state<Record<string, Record<string, number>>>({});
-  let tableByGrade = $state<Record<string, number>>({});
-  let teachers = $state<Set<string>>(new Set<string>());
-  let grades = $state<Set<string>>(new Set<string>());
-  let subjects = $state<Set<string>>(new Set<string>());
 
   const loadWorkload = async () => {
     if (timetableFile) {
@@ -28,12 +21,6 @@
         .then((newWeeklyLoad) => {
           step = 'checkLoadBySubject';
           weeklyLoad = newWeeklyLoad;
-          tableByTeacher = getTableByTeacher(newWeeklyLoad);
-          tableBySubject = getTableBySubject(newWeeklyLoad);
-          tableByGrade = getTableByGrade(newWeeklyLoad);
-          teachers = getTeachers(newWeeklyLoad);
-          grades = getGrades(newWeeklyLoad);
-          subjects = getSubjects(newWeeklyLoad);
         });
     }
   };
@@ -42,123 +29,32 @@
     const target = event.target as HTMLInputElement;
     timetableFile = target.files?.[0] || null;
   };
-
-  const getTableByTeacher = (
-    load: WeeklyLoad[]
-  ): Record<string, Record<string, number>> | never => {
-    // assert each pair of teacher and grade is unique
-    const teacherSubjectPairs = new Set<string>();
-    const table: Record<string, Record<string, number>> = {};
-
-    load.forEach((lesson) => {
-      const { teacherName, gradeName, subjectName, weeklyLoad } = lesson;
-      const teacherSubject = `${teacherName} ${gradeName} ${subjectName}`;
-      if (teacherSubjectPairs.has(teacherSubject)) {
-        throw new Error(`Duplicado de par de profesor y materia: ${teacherSubject}`);
-      }
-      teacherSubjectPairs.add(teacherSubject);
-
-      if (!table[teacherName]) {
-        table[teacherName] = {};
-      }
-
-      if (!table[teacherName][gradeName]) {
-        table[teacherName][gradeName] = 0;
-      }
-
-      table[teacherName][gradeName] += weeklyLoad;
-    });
-
-    return table;
-  };
-
-  const getTableByGrade = (load: WeeklyLoad[]): Record<string, number> | never => {
-    const table: Record<string, number> = {};
-
-    load.forEach((lesson) => {
-      const { gradeName, weeklyLoad } = lesson;
-      if (!table[gradeName]) {
-        table[gradeName] = 0;
-      }
-
-      table[gradeName] += weeklyLoad;
-    });
-
-    return table;
-  };
-
-  const getTableBySubject = (
-    load: WeeklyLoad[]
-  ): Record<string, Record<string, number>> | never => {
-    const table: Record<string, Record<string, number>> = {};
-
-    load.forEach((lesson) => {
-      const { gradeName, subjectName, weeklyLoad } = lesson;
-      if (!table[subjectName]) {
-        table[subjectName] = {};
-      }
-
-      if (!table[subjectName][gradeName]) {
-        table[subjectName][gradeName] = 0;
-      }
-
-      table[subjectName][gradeName] += weeklyLoad;
-    });
-
-    return table;
-  };
-
-  const getTeachers = (load: WeeklyLoad[]): Set<string> => {
-    return new Set(load.map((lesson) => lesson.teacherName));
-  };
-
-  const getGrades = (load: WeeklyLoad[]): Set<string> => {
-    return new Set(load.map((lesson) => lesson.gradeName));
-  };
-
-  const getSubjects = (load: WeeklyLoad[]): Set<string> => {
-    return new Set(load.map((lesson) => lesson.subjectName));
-  };
-
-  $inspect(tableByTeacher);
 </script>
 
 {#if step === 'uploadWeeklyLoad'}
   <WeeklyLoadUpload bind:timetableName bind:timetablePeriods {loadWorkload} {handleFileChange} />
-{:else if step === 'checkLoadBySubject'}
-  <Table
-    {grades}
-    {teachers}
-    {subjects}
-    {tableByTeacher}
-    {tableByGrade}
-    {tableBySubject}
-    by="subject"
-  />
-  <button
-    type="button"
-    class="w-fit rounded-[2px] bg-blue-500 px-3 py-1 text-[12px] font-medium text-white"
-    onclick={() => {
-      step = 'checkLoadByTeacher';
-    }}>Comenzar con el horario</button
-  >
-{:else if step === 'checkLoadByTeacher'}
-  <Table
-    {grades}
-    {teachers}
-    {subjects}
-    {tableByTeacher}
-    {tableByGrade}
-    {tableBySubject}
-    by="teacher"
-  />
-  <button
-    type="button"
-    class="w-fit rounded-[2px] bg-blue-500 px-3 py-1 text-[12px] font-medium text-white"
-    onclick={() => {
-      if (weeklyLoad) {
-        timetable.fromWeeklyLoad(weeklyLoad, timetablePeriods);
-      }
-    }}>Comenzar con el horario</button
-  >
+{:else if step === 'checkLoadBySubject' && weeklyLoad}
+  <div class="flex flex-col items-center gap-6">
+    <Table {weeklyLoad} by="subject" />
+    <button
+      type="button"
+      class="w-fit rounded-[2px] bg-blue-500 px-3 py-1 text-[12px] font-medium text-white"
+      onclick={() => {
+        step = 'checkLoadByTeacher';
+      }}>Comenzar con el horario</button
+    >
+  </div>
+{:else if step === 'checkLoadByTeacher' && weeklyLoad}
+  <div class="flex flex-col items-center gap-6">
+    <Table {weeklyLoad} by="teacher" />
+    <button
+      type="button"
+      class="w-fit rounded-[2px] bg-blue-500 px-3 py-1 text-[12px] font-medium text-white"
+      onclick={() => {
+        if (weeklyLoad) {
+          timetable.fromWeeklyLoad(weeklyLoad, timetablePeriods);
+        }
+      }}>Comenzar con el horario</button
+    >
+  </div>
 {/if}
