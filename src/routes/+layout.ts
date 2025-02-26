3eae54dc46f6d371daae83d3c14920c3d69213c6
Julio Gajardo <oilujulio@gmail.com>
2025-02-26 17:08:10 -0300

    FEAT: added analytics (#12)

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,10 @@
 import { createBrowserClient, createServerClient, isBrowser } from '@supabase/ssr';
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 import type { LayoutLoad } from './$types';
+import { dev } from '$app/environment';
+import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+injectAnalytics({ mode: dev ? 'development' : 'production' });
 
 export const load: LayoutLoad = async ({ fetch, data, depends }) => {
   depends('supabase:auth');
